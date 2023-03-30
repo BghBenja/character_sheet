3a337b54df564972ca18533b9c8454e2fd799c00
BghBenja <bghbenja@gmail.com>
2023-03-30 17:29:11 +0200

    Add score modifier calculator

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,23 @@
-console.log("hello js")
+init()
+
+function init() {
+    const stats = document.getElementsByClassName("stat");
+    for (const stat of stats) {
+        stat.addEventListener("change", scoreModifierCalculator);
+    }
+    const statmods = document.getElementsByClassName("statmod")
+    for (const statmod of statmods){
+        statmod.addEventListener("change", kacsa)
+    }
+}
+
+function scoreModifierCalculator(e) {
+    const modifier = Math.floor((e.currentTarget.value / 2) - 5);
+    e.currentTarget.parentElement.nextElementSibling.children[0].value = modifier;
+    
+}
+
+function kacsa(e) {
+    console.log(e.currentTarget.value)
+}
+
